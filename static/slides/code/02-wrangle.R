a205a7bd1f72c998ccf8dfb68ec28b00c6fabfcc
Juan Tellez <juan.f.tellez@gmail.com>
2021-10-04 17:07:52 -0700

    started slides, fixed spencer calendly link

---
+++ static/slides/code/02-wrangle.R
@@ -0,0 +1,21 @@
+# load libraries
+library(tidyverse)
+library(nycflights13)
+
+
+# make a smaller version of the flights dataset
+flights_small = 
+  flights %>% 
+  select(month, day, dep_time, sched_dep_time, arr_time, sched_arr_time, 
+         origin, dest, air_time, distance)
+
+
+
+# do your work here!
+
+
+
+
+
+# elections data
+elections = read_rds(url("https://www.dropbox.com/s/2ne4v0yhfgnw1wd/elections.rds?dl=1"))
---
+++ static/slides/code/merge county data and 2020.R
@@ -0,0 +1,27 @@
+# load libraries
+library(tidyverse)
+library(socviz)
+
+
+# 2020 data
+df = read_csv("/Users/juan/Dropbox/github/US_County_Level_Election_Results_08-20/2020_US_County_Level_Presidential_Results.csv")
+
+
+# pare down the datasets
+county_small = county_data %>% 
+  select(fips = id, name, state, census_region, pop, female, white, 
+         black, travel_time, land_area, hh_income, 
+         per_dem_2012, per_gop_2012, 
+         per_dem_2016, per_gop_2016)
+
+elec_2020 = df %>% 
+  select(fips = county_fips, per_gop_2020 = per_gop, per_dem_2020 = per_dem)
+
+
+# merge and drop unneeded ones
+merged = left_join(elec_2020, county_small, by = "fips") %>% 
+  relocate(c(per_gop_2020, per_dem_2020), .after = last_col()) 
+
+
+write_rds(merged, "/Users/juan/Dropbox/websites/poli-301/static/files/elections.rds")
+
