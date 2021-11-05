8aff383ca95d334ce20b6f707a511885c998bf8f
Juan Tellez <juan.f.tellez@gmail.com>
2021-11-04 17:14:58 -0700

    class today

--- static/files/causal.R
+++ static/files/causal.R
@@ -7,3 +7,9 @@ library(tidyverse)
 fake = tibble(x = rnorm(n = 100, mean = 10, sd = 2), 
               y = 2 * x)
 
+
+
+# make a confounded relationship
+tibble(south = sample(c(0, 1), size = 50, replace = TRUE), 
+       waffle = rnorm(n = 50, mean = 20, sd = 4) + 10*south,
+       divorce = rnorm(n = 50, mean = 20, sd = 2) + 8*south)
