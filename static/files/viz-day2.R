e92e407456492127a442940d9d15884994c9297e
Juan Tellez <juan.f.tellez@gmail.com>
2022-09-29 15:03:32 -0700

    week 2 day 2

---
+++ static/files/viz-day2.R
@@ -0,0 +1,33 @@
+# Day 2 -------------------------------------------------------------------
+library(tidyverse)
+library(socviz)
+library(ggthemes)
+library(gapminder)
+
+# look at econ data
+economics
+?economics # look at variable names
+
+
+# MAKE PLOT HERE
+
+
+
+
+# organ data: grouped histogram
+organdata
+?organdata
+
+# MAKE PLOT HERE
+
+
+# datasets you can use
+starwars
+gapminder
+elections_historic
+organdata
+storms
+midwest
+
+
+# MAKE PLOT HER
