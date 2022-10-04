9adc50a30a0d69e08073702c537de3ba11d0d3c4
Juan Tellez <juan.f.tellez@gmail.com>
2022-10-04 15:01:41 -0700

    week 2 day 1

---
+++ static/slides/code/wrangle-day1.R
@@ -0,0 +1,37 @@
+# load libraries
+library(tidyverse)
+library(socviz)
+library(juanr)
+
+
+
+# World leaders -----------------------------------------------------------
+
+# look at data
+leader
+?leader
+
+
+# do your work here
+
+
+
+# American Voter Bot ------------------------------------------------------
+
+
+# look at data
+bot
+?bot
+
+
+
+# do your work here!
+
+
+
+# Elections Analyst -------------------------------------------------------
+
+
+# look at data
+elections
+?elections
---
+++ static/slides/code/wrangle-day2.R
@@ -0,0 +1,21 @@
+# load libraries
+library(tidyverse)
+library(socviz)
+library(juanr)
+
+
+
+# climate -----------------------------------------------------------------
+
+# look at data
+climate
+?climate
+
+
+
+# elections ---------------------------------------------------------------
+
+# look at data
+elections
+?elections
+
