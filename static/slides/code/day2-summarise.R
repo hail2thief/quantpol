26a86ee9bdc7ded4b9761820785b598c10485310
Juan Tellez <juan.f.tellez@gmail.com>
2022-10-12 14:40:57 -0700

    ps3

---
+++ static/slides/code/day2-summarise.R
@@ -0,0 +1,22 @@
+# libraries
+library(tidyverse)
+library(juanr)
+library(fivethirtyeight)
+
+# trade data
+bob_ross
+?bob_ross
+
+# do your work here!
+
+
+# load data
+flying
+?flying
+
+
+# do your work here!
+
+
+
+
